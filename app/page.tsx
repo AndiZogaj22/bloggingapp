b92aeaaac8eff0cc98d87d3053fdf71a6aafbe2d
AndiZogaj22 <andiolsizogaj@gmail.com>
2024-04-12 14:27:20 +0200

    hero component

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import path from 'path'
 import matter from 'gray-matter'
 import Link from 'next/link'
 import { ModeToggle } from '@/components/ModeToggle'
+import Hero from '@/components/Hero'
+
+
 
 export default function Home() {
   const blogDir  = "blogs";
@@ -23,17 +26,18 @@ export default function Home() {
 
   })
   return (
-   <main className='flex flex-col'>
-    <h1 className='text-3xl font-bold'>
-      My Next.Js Blog Site
-    </h1>
+    <div className=" absolute inset-0 -z-10 bg-inherit bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px] h-full overflow-y-auto">
+    <main className="wrapper mx-auto h-screen w-9/12">
+  <Hero />
 
     <section className='py-10'>
-    <h2 className='text-2xl font-blod'>
-      Latest Blogs
-    </h2>
-<div><ModeToggle /></div>
-    <div className='py-2'>
+    <div className="flex justify-center py-4"> <ModeToggle /></div>
+
+    <h2 className='text-4xl pb-7 font-bold  mt-2 mb-5 flex justify-center bg-inherit'>
+          Latest Blogs
+        </h2>
+       
+    <div className='py-2 '>
       {blogs.map(blog =>(
         <Link href={'/blogs/' + blog.slug} passHref key={blog.slug}>
           <div className='py-2 flex justify-between align-middle gap-2'>
@@ -54,6 +58,12 @@ export default function Home() {
       ))}
     </div>
     </section>
+    <div>
+        <div className="my-4 flex justify-center p-12">
+          <p className="text-sm text-gray-500">All rights reserved &copy; 2024</p>
+        </div>
+      </div>
    </main>
+   </div>
   )
 }
